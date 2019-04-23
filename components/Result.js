491b819c6c98fd9fc7eab17b201edf9b51895f36
poojas1607 <30726158+poojas1607@users.noreply.github.com>
2019-04-23 15:18:46 +0530

    Update Result.js

--- components/Result.js
+++ components/Result.js
@@ -2,23 +2,24 @@ import React  from 'react';
 
 const Result = props => (
     <div className="weather__info">
+       
         {
-            props.Name  && <p className="weather__key"> Issue Name :
-                <span className="weather__value">  { props.Name }</span>
+            props.Name  && <p className="weather__key" style={{textAlign:'center',color:'white',fontFamily:'Abel',fontSize:'20px'}}> Issue Name :
+                <span className="weather__value" >  { props.Name }</span>
              </p>
         }
         {
-            props.Accuracy && <p className="weather__key"> Accuracy:
+            props.Accuracy && <p className="weather__key" style={{textAlign:'center',color:'white',fontFamily:'Abel',fontSize:'20px'}}> Accuracy:
                 <span className="weather__value"> { props.Accuracy }	</span>
             </p>
         }
         {
-            props.Cause && <p className="weather__key"> Cause:
+            props.Cause && <p className="weather__key" style={{textAlign:'center',color:'white',fontFamily:'Abel',fontSize:'20px'}}> Cause:
                 <span className="weather__value"> { props.Cause } </span>
             </p>
         }
         {
-            props.Ranking && <p className="weather__key"> Ranking:
+            props.Ranking && <p className="weather__key" style={{textAlign:'center',color:'white',fontFamily:'Abel',fontSize:'20px'}}> Ranking:
                 <span className="weather__value"> { props.Ranking } </span>
             </p>
         }
